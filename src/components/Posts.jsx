3d706673bc7a1331e608ec06b9c95b9a3e1b1207
f4ncyz4nz4 <f4ncyz4nz4@gmail.com>
2025-03-08 10:32:57 +0100

    add oski_stealer_string_decryption

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -9,7 +9,13 @@ function Posts(props) {
       description:
         "The Gozi String Decryptor challenge in the Zero2Automated course involves reverse engineering the string decryption function of the Gozi malware and replicating it in Python.",
     },
-    // { id: 2, title: "Title 2", date: "2023-02-15", description: "description" },
+    {
+      id: "oski_stealer_string_decryption",
+      title: "Oski Stealer String Decryption - Zero2Automated",
+      date: "2025-03-08",
+      description:
+        "The Oski Stealer String Decryption challenge, part of the Zero2Automated series, involves reverse engineering the string decryption routine from a Oski Stealer sample and developing a script to automate the decryption process.",
+    },
   ];
 
   return (
